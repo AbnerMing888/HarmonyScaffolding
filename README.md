6e3bbfa0f6fb3dc9ea4c24377ce40eba265e0677
AbnerMing <gw00261583@gwm.cn>
2023-11-15 11:31:57 +0800

    脚手架提交json转对象功能

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # HarmonyScaffolding
+
 鸿蒙端脚手架，自动生成代码，快速查找Api，一键模板，让学习与开发效率大幅度提高。
+
+
+
+## 最新提交
+
+<p align="center" style="font-size:12px;"><img src="images/harmony_json" width="300px" /><br/>json转对象功能</p>
+
+```
+更新时间：2023年11月15日
+更新日志：鸿蒙脚手架提交，完成json转对象功能。
+文章学习地址：
+```
+
+
+## 快速使用
+
+**温馨提示：只第一次需要执行三步，等前两步完成之后，以后只执行第三步即可，前两步无须再执行**
+
+### 第一步：使用git down下项目
+
+大家可以按照自己习惯的方式进行下载，比如使用IDE，使用TortoiseGit工具，或者使用命令，都行，以下是命令的形式下载：
+
+```
+
+git clone https://github.com/AbnerMing888/HarmonyScaffolding.git
+
+```
+
+### 第二步：执行下载安装包命令
+
+**安装 Electron**
+
+脚手架工具采用的是Electron进行开发的，它是一个使用 JavaScript、HTML 和 CSS 构建跨平台的桌面应用程序，既然要使用它，就需要进行安装，命令如下：
+
+
+```
+npm install --save-dev electron@^15.0.0
+```
+
+目前我是指定版本安装的，并配置在了package.json文件中devDependencies，主要用于开发阶段，大家可以和我的不一样，这个问题不大。
+
+**安装模块remote**
+
+remote 模块提供了一种在渲染进程和主进程之间进行进程间通讯的简便途径，使用 remote 模块，可以调用主进程对象的方法，而无需显式地发送进程间消息，这类似于 Java 的 RMI，虽然说13版本之后禁用了Remote模块，但是也提供了使用一个新的包 @electron/remote 来替代，一句话，主要用于dialog相关，比如弹窗，选择电脑路径等。
+
+```
+npm i -D @electron/remote
+
+```
+
+### 第三步：根项目下执行命令
+
+执行完上述两步之后，以后在进入项目，只执行如下命令即可，使用此命令的前提，你已经安装好了鸿蒙的开发环境了，并也配好了环境变量。
+
+```
+npm start
+```
+
+
+### 欢迎关注作者
+
+微信搜索【App开发干货铺】，或扫描下面二维码关注，查阅更多技术文章！
+
+<img src="images/abner.jpg" width="200px" />
+
+### 赞赏作者
+
+看在作者这么努力的份上，微信赞赏随意，给个鼓励好不好~
+
+<img src="images/wx_code.jpg" width="200px" />
+
+### License
+
+```
+Copyright (C) AbnerMing, HarmonyScaffolding Open Source Project
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
+
+
---
+++ api/awesomplete.js
@@ -0,0 +1,294 @@
+/**
+ * Simple, lightweight, usable local autocomplete library for modern browsers
+ * Because there weren鈥檛 enough autocomplete scripts in the world? Because I鈥檓 completely insane and have NIH syndrome? Probably both. :P
+ * @author Lea Verou http://leaverou.github.io/awesomplete
+ * MIT license
+ */
+
+(function() {
+
+    function $(expr, con) {
+        if (!expr) return null;
+        return typeof expr === "string" ? (con || document).querySelector(expr) : expr;
+    }
+
+    function $$(expr, con) {
+        return Array.prototype.slice.call((con || document).querySelectorAll(expr));
+    }
+
+    $.create = function(tag, o) {
+        var element = document.createElement(tag);
+
+        for (var i in o) {
+            var val = o[i];
+
+            if (i == "inside") {
+                $(val).appendChild(element);
+            } else if (i == "around") {
+                var ref = $(val);
+                ref.parentNode.insertBefore(element, ref);
+                element.appendChild(ref);
+            } else if (i in element) {
+                element[i] = val;
+            } else {
+                element.setAttribute(i, val);
+            }
+        }
+
+        return element;
+    };
+
+    $.bind = function(element, o) {
+        if (element) {
+            for (var event in o) {
+                var callback = o[event];
+
+                event.split(/\s+/).forEach(function(event) {
+                    element.addEventListener(event, callback);
+                });
+            }
+        }
+    };
+
+    $.fire = function(target, type, properties) {
+        var evt = document.createEvent("HTMLEvents");
+
+        evt.initEvent(type, true, true);
+
+        for (var j in properties) {
+            evt[j] = properties[j];
+        }
+
+        target.dispatchEvent(evt);
+    };
+
+    var _ = self.Awesomplete = function(input, o) {
+        var me = this;
+
+        // Setup environment
+        o = o || {};
+
+        this.input = input;
+        input.setAttribute("aria-autocomplete", "list");
+
+        this.minChars = +input.getAttribute("data-minchars") || o.minChars || 1;
+        this.maxItems = +input.getAttribute("data-maxitems") || o.maxItems || 10;
+
+        if (input.hasAttribute("list")) {
+            this.list = "#" + input.getAttribute("list");
+            input.removeAttribute("list");
+        } else {
+            this.list = input.getAttribute("data-list") || o.list || [];
+        }
+
+        this.filter = o.filter || _.FILTER_CONTAINS;
+        this.sort = o.sort || _.SORT_BYLENGTH;
+
+        this.autoFirst = input.hasAttribute("data-autofirst") || o.autoFirst || false;
+
+        this.item = o.item || function(text, input) {
+            return $.create("li", {
+                innerHTML: text.replace(RegExp(regEscape(input.trim()), "gi"), "<mark>$&</mark>"),
+                "aria-selected": "false"
+            });
+        };
+
+        this.index = -1;
+
+        this.container = $.create("div", {
+            className: "awesomplete",
+            around: input
+        });
+
+        this.ul = $.create("ul", {
+            hidden: "",
+            inside: this.container
+        });
+
+        // Bind events
+
+        $.bind(this.input, {
+            "input": me.evaluate.bind(me),
+            "blur": me.close.bind(me),
+            "keydown": function(evt) {
+                var c = evt.keyCode;
+
+                if (c == 13 && me.index > -1) { // Enter
+                    evt.preventDefault();
+                    me.select();
+                } else if (c == 27) { // Esc
+                    me.close();
+                } else if (c == 38 || c == 40) { // Down/Up arrow
+                    evt.preventDefault();
+                    me[c == 38 ? "previous" : "next"]();
+                }
+            }
+        });
+
+        $.bind(this.input.form, { "submit": me.close.bind(me) });
+
+        $.bind(this.ul, {
+            "mousedown": function(evt) {
+                var li = evt.target;
+
+                if (li != this) {
+
+                    while (li && !/li/i.test(li.nodeName)) {
+                        li = li.parentNode;
+                    }
+
+                    if (li) {
+                        me.select(li);
+                    }
+                }
+            }
+        });
+    };
+
+    _.prototype = {
+        set list(list) {
+            if (Array.isArray(list)) {
+                this._list = list;
+            } else {
+                if (typeof list == "string" && list.indexOf(",") > -1) {
+                    this._list = list.split(/\s*,\s*/);
+                } else {
+                    list = $(list);
+
+                    if (list && list.children) {
+                        this._list = [].slice.apply(list.children).map(function(el) {
+                            return el.innerHTML.trim();
+                        });
+                    }
+                }
+            }
+        },
+
+        close: function() {
+            this.ul.setAttribute("hidden", "");
+            this.index = -1;
+
+            $.fire(this.input, "awesomplete-close");
+        },
+
+        open: function() {
+            this.ul.removeAttribute("hidden");
+
+            if (this.autoFirst && this.index == -1) {
+                this.goto(0);
+            }
+
+            $.fire(this.input, "awesomplete-open");
+        },
+
+        next: function() {
+            var count = this.ul.children.length;
+
+            this.goto(this.index < count - 1 ? this.index + 1 : -1);
+        },
+
+        previous: function() {
+            var count = this.ul.children.length;
+
+            this.goto(this.index > -1 ? this.index - 1 : count - 1);
+        },
+
+        // Should not be used, highlights specific item without any checks!
+        goto: function(i) {
+            var lis = this.ul.children;
+
+            if (this.index > -1) {
+                lis[this.index].setAttribute("aria-selected", "false");
+            }
+
+            this.index = i;
+
+            if (i > -1 && lis.length > 0) {
+                lis[i].setAttribute("aria-selected", "true");
+            }
+        },
+
+        select: function(selected) {
+            selected = selected || this.ul.children[this.index];
+
+            if (selected) {
+                var prevented;
+
+                $.fire(this.input, "awesomplete-select", {
+                    text: selected.textContent,
+                    preventDefault: function() {
+                        prevented = true;
+                    }
+                });
+
+                if (!prevented) {
+                    this.input.value = selected.textContent;
+                    this.close();
+                    $.fire(this.input, "awesomplete-selectcomplete");
+                }
+            }
+        },
+
+        evaluate: function() {
+            var me = this;
+            var value = this.input.value;
+
+            if (value.length >= this.minChars && this._list.length > 0) {
+                this.index = -1;
+                // Populate list with options that match
+                this.ul.innerHTML = "";
+
+                this._list.filter(function(item) {
+                    return me.filter(item, value);
+                })
+                    .sort(this.sort)
+                    .every(function(text, i) {
+                        me.ul.appendChild(me.item(text, value));
+
+                        return i < me.maxItems - 1;
+                    });
+
+                this.open();
+            } else {
+                this.close();
+            }
+        }
+    };
+
+    _.FILTER_CONTAINS = function(text, input) {
+        return RegExp(regEscape(input.trim()), "i").test(text);
+    };
+
+    _.FILTER_STARTSWITH = function(text, input) {
+        return RegExp("^" + regEscape(input.trim()), "i").test(text);
+    };
+
+    _.SORT_BYLENGTH = function(a, b) {
+        if (a.length != b.length) {
+            return a.length - b.length;
+        }
+
+        return a < b ? -1 : 1;
+    };
+
+    function regEscape(s) {
+        return s.replace(/[-\\^$*+?.()|[\]{}]/g, "\\$&"); }
+
+    function init() {
+        $$("input.awesomplete").forEach(function(input) {
+            new Awesomplete(input);
+        });
+    }
+
+    // DOM already loaded?
+    if (document.readyState !== "loading") {
+        init();
+    } else {
+        // Wait for it
+        document.addEventListener("DOMContentLoaded", init);
+    }
+
+    _.$ = $;
+    _.$$ = $$;
+
+})();
